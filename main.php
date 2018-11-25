4433ed8e450778178768c41b3ec067a378859f59
loicbtd <loic.bert.marcel@gmail.com>
2018-11-25 19:59:58 +0100

    ebauche header+aside

---
+++ main.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="fr" >
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/mystyle.css">
+    <title>YouTube - Un Géant du Web</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+      <!-- header -->
+      <header class="row">
+        <!-- col logo and title -->
+        <div class="col-md-3">
+          <div class="row">
+            <!-- col logo -->
+            <div class="col-md-3 element-center">
+              <img src="img/projet_logo.png" alt="logo du projet" width="50" height="50">
+            </div>
+            <!-- col title -->
+            <div class="col-md-9">
+              <h1>YouTube</h1>
+              <h2>Un Géant du Web</h2>
+            </div>
+          </div>
+        </div>
+        <!-- col empty -->
+        <div class="col-md-2"></div>
+        <!-- col searchbar -->
+        <div class="col-md-3 element-center">
+          <form class="post" action="index.html" method="post">
+            <input type="text" name="searchbar" placeholder="Saisissez votre recherche">
+          </form>
+        </div>
+        <!-- col empty -->
+        <div class="col-md-4"></div>
+      </header>
+
+      <div class="row">
+        <aside class="col-md-2">
+          <div class="row">
+            <div class="col-md-12 element-center">
+              <img src="img/yt_logo.png" alt="logo du projet" width="50" height="50">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 element-center">
+              <h1>YouTube LLC</h1>
+              <h2>San Bruno, CA </h2>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 element-center">
+              <h1>999 MM</h1>
+              <h2>Vidéos</h2>
+            </div>
+            <div class="col-md-6 element-center">
+              <h1>2 MM</h1>
+              <h2>Abonnés</h2>
+            </div>
+          </div>
+          <div class="row">
+            <hr class="col-md-8">
+          </div>
+          <div class="row">
+            <ul>
+              <li class="histoire"><a href="#">Histoire</a></li>
+              <li class="poids"><a href="#"> Le poids dans la société</a></li>
+              <li class="danger"><a href="#"> Les dangers et limites</a></li>
+              <li class="metier"><a href="#"> Le métier de youtubeur</a></li>
+              <li class="devenir"><a href="#"> Devenir youtubeur</a></li>
+              <li class="contact"><a href="#"> Contactez-nous</a></li>
+            </ul>
+          </div>
+          <div class="row">
+            <hr class="col-md-8">
+          </div>
+        </aside>
+
+        <main class="col-md-7">
+
+        </main>
+
+        <aside class="col-md-3">
+
+        </aside>
+
+      </div>
+
+    </div>
+  </body>
+</html>
