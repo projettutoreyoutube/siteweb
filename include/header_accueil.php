0bcb3bbd3b623992ba63a1b0a4dab8bf16bdada9
tomjcb <tom.jacob@outlook.fr>
2018-12-09 00:06:33 +0100

    ajout d'un switch case pour la selection des bar_nav

--- include/header_accueil.php
+++ include/header_accueil.php
@@ -1,4 +1,8 @@
 <!-- header -->
+<?php if(isset($_GET['partie'])){
+  $partie = $_GET['partie'];
+} ?>
+
 <div class="header_tech">
 	<header class="row">
 		<div class="col-sm-12 banner">
@@ -9,30 +13,27 @@
 </div>
 
 <div class="header_accueil">
-  <header class="row">
-    <!-- col logo and title -->
-    <div class="col-md-3">
-      <div class="row">
-        <!-- col logo -->
-        <div class="col-xs-3 col-sm-1 col-md-4 col-lg-3 element-center">
-          <a href="index.php"><img src="img/projet_logo.png" alt="logo du projet" width="50" height="50"></a>
-        </div>
-        <!-- col title -->
-        <div class="col-xs-3 col-sm-5 col-md-7 col-lg-6 element-center">
-          <h1>YouTube</h1>
-          <h2>Un Géant du Web</h2>
-        </div>
-      </div>
-    </div>
-    <!-- col empty -->
-    <div class="col-md-2"></div>
-    <!-- col searchbar -->
-    <div class="col-xs-3 col-sm-3 col-md-3 element-center">
-      <form class="post" action="index.php" method="post">
-        <input class="search" type="text" name="searchbar" placeholder="Saisissez votre recherche">
-      </form>
-    </div>
-    <!-- col empty -->
-    <div class="col-md-4"></div>
-  </header>
+	<?php
+	switch($partie){
+		case 1:
+		include('include/navigation/nav_histoire.php');
+		break;
+
+		case 2:
+		include('include/navigation/nav_societe.php');
+		break;
+
+		case 3:
+		include('include/navigation/nav_dangers.php');
+		break;
+
+		case 4:
+		include('include/navigation/nav_metier.php');
+		break;
+
+		case 5:
+		include('include/navigation/nav_devenir.php');
+		break;
+	}
+	?>
 </div>
