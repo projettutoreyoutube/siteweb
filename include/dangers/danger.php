85a061417d9f6fc31846900ea4b229ecf2056775
tomjcb <tom.jacob@outlook.fr>
2018-12-09 00:04:26 +0100

    Suppresiondes barnav dans les pages
    Je les supprime pour les mettre dans des fichier séparés.
    Voir mon commit sur les bar nav pour + d'infos

--- include/dangers/danger.php
+++ include/dangers/danger.php
@@ -6,15 +6,6 @@ else{
   $subpart = 0;
 }
 ?>
-
-<div class="row">
-<nav class="navigation">
-  <a class="col-md-3 partie" href="index.php?partie=1&subpart=1">Partie1</a>
-  <a class="col-md-3 partie" href="index.php?partie=1&subpart=2">Partie2</a>
-  <a class="col-md-3 partie" href="index.php?partie=1&subpart=3">Partie3</a>
-  <a class="col-md-3 partie" href="index.php?partie=1&subpart=4">Partie4</a>
-</nav>
-</div>
 <?php switch($subpart){
   case 0:
   include('include/dangers/presentation.php');
