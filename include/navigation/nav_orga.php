00f36d1e2fc162632b9a48776e45d54a8c64ecac
tomjcb <tom.jacob@outlook.fr>
2018-12-09 22:50:42 +0100

    Ajout du contenu dans la partie orga
    J'en ai marre

--- include/navigation/nav_orga.php
+++ include/navigation/nav_orga.php
@@ -2,9 +2,10 @@
 <div class="nav-header">
 	<div class="container top">
 		<nav class="col-md-12 nav ">
-		  <a class="col-md-4 top" href="index.php?partie=6&subpart=1">Taille et chiffre d'affaires</a>
-		  <a class="col-md-4 top" href="index.php?partie=6&subpart=2">Fonctionnalités premium</a>
-		  <a class="col-md-4 top" href="index.php?partie=6&subpart=3">Choix des contenus</a>
+		  <a class="col-md-3 top" href="index.php?partie=6&subpart=1">Taille et chiffre d'affaires</a>
+		  <a class="col-md-3 top" href="index.php?partie=6&subpart=2">Critères juridiques</a>
+		  <a class="col-md-3 top" href="index.php?partie=6&subpart=3">Capitaux, nationalité et cotation en bourse</a>
+			<a class="col-md-3 top" href="index.php?partie=6&subpart=4">Critères économiques</a>
 		</nav>
 	</div>
 </div>
