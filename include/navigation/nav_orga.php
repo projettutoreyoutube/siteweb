0f2034343060da63a0673ceea625eb5eca608e51
tomjcb <tom.jacob@outlook.fr>
2018-12-09 23:42:55 +0100

    ajout de menu ultra responsive +++++ sucez moi

--- include/navigation/nav_orga.php
+++ include/navigation/nav_orga.php
@@ -1,11 +1,12 @@
 
-<div class="nav-header">
+<div class="d-none d-md-block col-md-12 nav-header">
 	<div class="container top">
-		<nav class="col-md-12 nav ">
+		<nav class="nav ">
 		  <a class="col-md-3 top" href="index.php?partie=6&subpart=1">Taille et chiffre d'affaires</a>
 		  <a class="col-md-3 top" href="index.php?partie=6&subpart=2">Critères juridiques</a>
 		  <a class="col-md-3 top" href="index.php?partie=6&subpart=3">Capitaux, nationalité et cotation en bourse</a>
 			<a class="col-md-3 top" href="index.php?partie=6&subpart=4">Critères économiques</a>
 		</nav>
+
 	</div>
 </div>
