14df6a0d891a9800793f358162aa8e95bce1b853
loicbtd <loic.bert.marcel@gmail.com>
2018-12-09 18:08:00 +0100

    ajout caroussel

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.js"></script>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/mystyle.css">
     <title>YouTube - Un Géant du Web</title>
