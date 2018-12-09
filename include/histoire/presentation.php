ef0c01f7495b3aee34d5282ed3623f5bb3f005ba
tomjcb <tom.jacob@outlook.fr>
2018-12-09 15:18:50 +0100

    ajout d'un menu de nav avec des image a l'accueil
    et ajout d'image dans chaucn des parties, cela est vraiment swag

--- include/histoire/presentation.php
+++ include/histoire/presentation.php
@@ -1,16 +1,13 @@
 <div class="corps_texte">
   <div class="main_title">
-    Ceci est une présentation de grande partie (Histoire)
-  </div>
-  <div class="main_subtitle">
-    Blah blah blah vraiment intéréssant
+    Bienvenue sur la partie histoire de YouTube
   </div>
   <div class="content">
     Cette partie du site a pour but de présenter l'entreprise YouTube LLC d'un point de vue histoire : vous pourrez y trouver quelques détails sur sa création ainsi que son parcours au fil des années
   </div>
   <div class="row">
   <div class="img-md">
-    <img class="banner" src="img/money_backgroung.jpg" alt="image barre"/>
+    <img class="presentation" src="img/histoire/histoire_presentation.jpg" alt="image barre"/>
   </div>
 </div>
 </div>
--- include/presentation.php
+++ include/presentation.php
@@ -1,3 +1,44 @@
+<p class="col-sm-12 paragraph">
+	<div class="row col-lg-12 miniatures">
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="index.php?partie=1"><img class="background" src="img/histoire/histoire_presentation.jpg" alt="background pour lien"></a>
+				<a class="raccourci" href="index.php?partie=1">Histoire</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="index.php?partie=2"><img class="background" src="img/poids/poids_presentation.jpg" alt="background pour lien"></a>
+				<a class="raccourci" href="index.php?partie=2">Poids dans la société</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="index.php?partie=3"><img class="background" src="img/dangers/dangers_presentation.jpeg" alt="background pour lien"></a>
+				<a class="raccourci" href="index.php?partie=3">Les dangers et les limites</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="index.php?partie=4"><img class="background" src="img/metier/metier_presentation.png" alt="background pour lien"></a>
+				<a class="raccourci" href="index.php?partie=4">Notre équipe</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="page.php"><img class="background" src="img/miniatures/banner-minia.png" alt="background pour lien"></a>
+				<a class="raccourci" href="page.php">Partie Technique</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-6 raccourci">
+			<div class="lien">
+				<a class="img" href="page.php?article=4"><img class="background" src="img/miniatures/contact-minia.jpg" alt="background pour lien"></a>
+				<a class="raccourci" href="page.php?article=4">Nous contacter</a>
+			</div>
+		</div>
+</div>
+</p>
+
 <div class="slideshow-container">
   <div class="mySlides fade">
     <a href="http://www.iut-bm.univ-fcomte.fr/pages/fr/dpt-info---actualites-17109.html"><img src="img/logo_info.png" width="80%" height="20%"></a>
