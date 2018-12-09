cf2f168bb34940ecc4873bb02e2f09aac3ddbbb8
loicbtd <loic.bert.marcel@gmail.com>
2018-12-10 00:59:56 +0100

    add Erwan's presentation

--- include/technique/groupe.php
+++ include/technique/groupe.php
@@ -59,12 +59,7 @@
 <p class="col-sm-12 paragraph">
 	<img src="img/photo/erwan.jpg" alt="photo d'Erwan" class="photo">
 	<br>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	Je suis Erwanomax, j'ai 18 ans.
 </p>
 
 <div class="row col-md-12">
