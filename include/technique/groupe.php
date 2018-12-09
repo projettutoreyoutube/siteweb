e88328ada9cca57958aacbd896534a5c119d7f73
Quentin <quedel98@gmail.com>
2018-12-09 17:34:47 +0100

    G mi lé foto

--- include/technique/groupe.php
+++ include/technique/groupe.php
@@ -1,9 +1,10 @@
 <style>
 	img.photo{
-		width:100px;
+		width:150px;
 		float: left;
 		margin-right: 15px;
 		padding-top:7px;
+		border-radius: 50%;
 	}
 </style>
 
@@ -13,7 +14,7 @@
 
 <h4 class="nom">Chef de projet : Loïc Bertrand</h3>
 <p class="col-sm-12 paragraph">
-	<img src="img/photo.png" alt="photo de Loïc" class="photo">
+	<img src="img/photo/loic.jpg" alt="photo de Loïc" class="photo">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -24,7 +25,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 <h4 class="nom">Membre : Quentin Delignou</h3>
 <p class="col-sm-12 paragraph">
-	<img src="img/photo.png" alt="photo de Quentin" class="photo">
+	<img src="img/photo/quentin.jpg" alt="photo de Quentin" class="photo">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -35,7 +36,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 <h4 class="nom">Membre : Tom Jacob</h3>
 <p class="col-sm-12 paragraph">
-	<img src="img/photo.png" alt="photo de Tom" class="photo">
+	<img src="img/photo/tom.jpg" alt="photo de Tom" class="photo">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -46,7 +47,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
 <h4 class="nom">Membre : Erwan Vinot</h3>
 <p class="col-sm-12 paragraph">
-	<img src="img/photo.png" alt="photo d'Erwan" class="photo">
+	<img src="img/photo/erwan.jpg" alt="photo d'Erwan" class="photo">
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
