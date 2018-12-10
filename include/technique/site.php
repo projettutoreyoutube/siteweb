42b0cc589b345f842148862cc9aeba1397c6a94a
loicbtd <loic.bert.marcel@gmail.com>
2018-12-10 01:31:44 +0100

    page avis sur le site loic

--- include/technique/site.php
+++ include/technique/site.php
@@ -7,14 +7,25 @@
 
 <h4 class="nom">Remarques de Loïc Bertrand</h4>
 <p class="col-sm-12 paragraph">
+	La réalisation de ce site a été l'occasion parfaite d'appliquer les connaissances de développement web accumulées depuis le début de l'année. Mais cela nous a également permis de cultiver, de notre propre initiative, des notions comme l'utilisation de solutions de versionnage de code.
+	<br>
+	La réalisation de ce site web s'est inscrite comme une première expérience de codage en groupe pour moi. La mise en place d'un repository GitHub nous a grandement facilité la tâche.
+	<br><br>
+	<span>Difficultés rencontrées :</span><br><br>
+	J'ai rencontré quelques difficultés lors du codage du site.
+	<br>
+	J'ai dû prendre le temps d'apprendre le fonctionnement de GitHub afin maîtriser la gestion des conflits entre les différentes branches de développement. J'ai ensuite pu proposer cette solution aux autres membres du groupe qui l'ont tout de suite adoptée.
+	<br>
+	J'ai également rencontré quelques problèmes lors du codage des scripts javascript du caroussel d'accueil et de la fonction qui permet un retour en haut de la page. Cependant, cela m'a permis de comprendre la logique de ce langage de programmation que je ne connaissais pas.
+
 
 </p>
 
 <h4 class="nom">Remarques de Quentin Delignou</h4>
 <p class="col-sm-12 paragraph">
 	Le rendu du site est joli, pas aussi incroyable que ce que j'avais imaginé au départ, mais selon moi bien réussi dans la mesure où on a seulement fait 4 mois de développement web. Je suis donc très satisfait du travail réalisé par l'équipe.
-	<br /><br />
-	<strong>Difficultés rencontrés :</strong>
+	<br><br>
+	<span>Difficultés rencontrées :</span>
 	<li class="paragraph">Positionnement de la barre de navigation lors du scrollage : le but de la barre de navigation est qu'elle reste en place en haut de l'écran lorsque l'utilisateur descend la page. Le problème est que, quand j'ai cherché comment faire, je n'ai trouvé que des solutions utilisant des scripts javascripts, notamment la fonction affix. Comme je ne maitrise pas le javascript, je n'ai pas réussi à l'utiliser. Mr Trannoy m'a orienté sur la propriété css "sticky" pour la position de la barre, et elle reste maintenant en haut l'écran pendant le scrolling (même si l'utilisation de sticky oblige à changer le code d'une manière un peu moins propre, et le positionnement n'est pas parfait).</li>
 	<li class="paragraph">Animation lors du retour en haut de la page : mon but était de faire un bouton "retour en haut" avec une animation de scroll inversé lors du retour en haut. Le problème est que ce bouton fonctionne avec des scripts javascript que je ne maitrise pas. J'ai bien tenté un copié/collé des codes d'exemple mais cela n'a rien donné.</li>
 	<li class="paragraph">Page de contact : pour la page de contact, le but était de faire un formulaire d'envoi de mail fonctionnel, pas juste une interface graphique. La tâche s'est révélée assez ardue. J'ai commencé par regarder des codes php à écrire pour réaliser ce type de formulaire (des codes avec 300 lignes de php), on m'a ensuite parlé de l'application PHP Mailer que j'ai essayé de mettre en place.</li>
@@ -23,8 +34,8 @@
 <h4 class="nom">Remarques de Tom Jacob</h4>
 <p class="col-sm-12 paragraph">
 	Même si à la base, nous avions pleins d'idées quand nous avons réalisé notre prototype, nous avons eu énormément d'autres idées pendant la réalisation. Ce site étant mon premier, je reste quand même très fier de mon travail et surtout de notre travail à tous.
-	<br /><br />
-	<strong>Difficultés rencontrés :</strong>
+	<br><br>
+	<span>Difficultés rencontrées :</span>
 	<li class="paragraph">Tout comme Quentin, je voulais positionner la barre de navigation en haut lors du scrollage : c'est à dire que lorsque l'on scroll vers le bas, la barre de navigation reste "collée" en haut. A l'aide des recherches de Quentin, j'ai pu réaliser, non sans mal, cette fonctionnalité.
 	<br> Je n'ai pas eu d'autres problèmes particuliers durant la réalisation de mes parties, si ce n'est des problèmes d'affichage CSS, ou de fautes de frappes qui faisaient beuguer le PHP.</li>
 	</p>
